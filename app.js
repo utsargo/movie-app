29534d1793d59e30dc3826b0ee8751e2408d630c
utsargo <royutsargo@gmail.com>
2019-01-04 15:48:25 +0600

    added files

---
+++ app.js
@@ -0,0 +1,24 @@
+const MovBaseUrl = "https://api.themoviedb.org/3/movie/";
+const ApiKey = "298f5c948b209ca206b7885a546e697e";
+
+function buildUrl(url) {
+    return MovBaseUrl + url + ".json?api-key=" + ApiKey
+}
+
+const vm = new Vue({
+    el: '#app',
+    data: {
+        results: []
+    },
+    mounted() {
+        this.getPosts('home');
+    },
+    methods: {
+        getPosts(section) {
+            let url = buildUrl(section);
+            axios.get(url).then((response) => {
+                this.results = response.data.results;
+            }).catch(error => { console.log(error); });
+        }
+    }
+});
